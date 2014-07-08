0bcb6c073f31623d9305d82349788584c7056c87
Gernot Ullrich <gernot.ullrich@gmx.de>
2014-07-08 09:12:41 +0200

    add capistrano

--- Gemfile
+++ Gemfile
@@ -1,15 +1,17 @@
-source :rubygems
+source "https://rubygems.org"
+
+
 
 gem 'dynamic_form', '1.1.4'
 gem 'flutie', '1.3.3'
 gem 'bourbon', '2.1.0'
 gem 'formtastic', '2.0.2'
 gem 'htmldiff', '0.0.1'
-gem 'pg', '0.13.2'
-gem 'rails', '3.2.6'
+gem 'pg'
+gem 'rails', '3.2.11'
 gem 'thin', '1.3.1'
 gem 'yajl-ruby', '1.1.0', :require => 'yajl'
-gem "jquery-rails", "1.0.19"
+gem 'jquery-rails', '>= 1.0.3'
 
 group :assets do
   gem 'sass-rails', "~> 3.2.5"
@@ -18,6 +20,12 @@ group :assets do
   gem "yui-compressor"
 end
 
+# Deploy with Capistrano
+gem 'capistrano', '~> 3.0'
+gem 'capistrano-ext'
+gem 'capistrano-rails'
+gem 'capistrano-bundler'
+gem 'capistrano-rvm', '~> 0.0.3'
 
 group :development, :test do
   gem 'foreman', '0.40.0'
@@ -28,7 +36,7 @@ end
 
 group :test do
   gem 'bourne', '1.1.1'
-  gem 'capybara-webkit', '0.12.1'
+  #gem 'capybara-webkit', '0.12.1'
   gem 'cucumber-rails', '1.3.0', :require => false
   gem 'database_cleaner', '0.7.1'
   gem 'factory_girl_rails', '1.7.0'
---
+++ config/deploy.rb
@@ -0,0 +1,55 @@
+set :application, 'copycopter'
+set :repo_url, 'git@github.com:GernotUllrich/copycopter-server.git'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/var/www/copycopter'
+# set :scm, :git
+
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
+
+set :bundle_flags, '--deployment'
+set :bundle_roles, :all
+set :default_env, {rvm_bin_path: '~/.rvm/bin'}
+set :rake, 'bundle exec rake'
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+  desc 'Provision env before assets:precompile'
+  task :fix_bug_env do
+    set :rails_env, (fetch(:rails_env) || fetch(:stage))
+  end
+
+  before 'deploy:assets:precompile', 'deploy:fix_bug_env'
+  before 'deploy:migrate', 'deploy:fix_bug_env'
+
+
+end
